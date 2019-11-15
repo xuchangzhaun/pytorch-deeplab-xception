267ac49e7f0d52e87727297f8c228fa0721ec150
jcoral <1355539304@qq.com>
2019-11-15 10:29:00 +0800

    Update calculate_weights.py

--- utils/calculate_weights.py
+++ utils/calculate_weights.py
@@ -23,7 +23,7 @@ def calculate_weigths_labels(dataset, dataloader, num_classes):
         class_weight = 1 / (np.log(1.02 + (frequency / total_frequency)))
         class_weights.append(class_weight)
     ret = np.array(class_weights)
-    classes_weights_path = os.path.join(Path.db_root_dir(dataset), dataset+'_classes_weights.npy')
+    classes_weights_path = os.path.join('class_weight', dataset+'_classes_weights.npy')
     np.save(classes_weights_path, ret)
 
     return ret
